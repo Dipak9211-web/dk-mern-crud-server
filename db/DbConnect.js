757615de4d2fdf07dcb5685496dab3abaca1f3e2
Dipak Kumar <kumardipak4191@gmail.com>
2022-10-13 20:23:29 +0530

    thired change in dbConnection

--- db/DbConnect.js
+++ db/DbConnect.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
 const URL = `${process.env.DB_URL}`
- mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
-    if(err){
-        console.log("unable to connect the database")
-        console.log(err)
-    }else{
-        console.log("successfully connected to the database")
-    }
- })
+const mongoose = require('mongoose');
+const dbfunction = async()=>{
+  await mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
+        if(err){
+            console.log("unable to connect the database")
+            console.log(err)
+        }else{
+            console.log("successfully connected to the database")
+        }
+     })
+}
+dbfunction();
+ 
