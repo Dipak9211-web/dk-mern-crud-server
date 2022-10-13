132066d1cf2dee2639fad4f926597ed08fcfb7cb
Dipak Kumar <kumardipak4191@gmail.com>
2022-10-13 20:09:45 +0530

    one change in dbConnection file

--- db/DbConnect.js
+++ db/DbConnect.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
-const username = process.env.DB_USER_NAME
-const adminpassword = process.env.DB_PASSWORD
-const URL = `mongodb+srv://${username}:${adminpassword}@cluster0.jmu49dr.mongodb.net/${process.env.DB_COLLECTION_NAME}?retryWrites=true&w=majority`
+const URL = `${process.env.DB_URL}`
  mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
     if(err){
         console.log("unable to connect the database")
